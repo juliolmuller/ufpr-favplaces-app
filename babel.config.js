98e17225ee5312fbbb9eaa5eaba248d247ff72aa
Julio Muller <juliodolago@gmail.com>
2021-02-24 23:16:54 -0300

    COnfigured Babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
-  api.cache(true);
+/* eslint-env node */
+module.exports = function (api) {
+  api.cache(true)
+
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: [
+      'babel-preset-expo',
+    ],
+    plugins: [
+      'babel-plugin-jsx-control-statements',
+    ],
+  }
+}
