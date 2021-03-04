0ac0281d987166aa965942327c1f277d11f21d96
Julio Muller <juliodolago@gmail.com>
2021-03-04 01:21:22 -0300

    Created start screen with map and markers

--- src/components/PlacesMap/index.jsx
+++ src/components/PlacesMap/index.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Text, View } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import mapMarker from '../../assets/map-marker.png'
+import http from '../../services/http'
 import styles from './styles'
 
 function PlacesMap() {
+  const { navigate } = useNavigation()
+  const [places, setPlaces] = useState([])
+
+  const fetchPlaces = async () => {
+    try {
+      const { data } = await http.get('/places')
+      setPlaces(data)
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPlaces()
+    }, []),
+  )
+
   return (
-    <View style={styles.wrapper}>
-      <Text>PlacesMap</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -25.425,
+          longitude: -49.275,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      >
+        {places.map((place) => (
+          <Marker
+            key={place.id}
+            icon={mapMarker}
+            coordinate={{ latitude: place.latitude, longitude: place.longitude }}
+            calloutAnchor={{ x: 2.7, y: 0.8 }}
+          >
+            <Callout tooltip onPress={() => navigate('PlaceDetails', place)}>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>{place.name}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          {places.length} lugares marcados
+        </Text>
+        <MaterialIcons name="stars" size={24} color="#fff" />
+      </View>
     </View>
   )
 }
--- src/components/PlacesMap/styles.js
+++ src/components/PlacesMap/styles.js
@@ -1,9 +1,49 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
+import * as theme from '../../theme'
 
 export default StyleSheet.create({
-  wrapper: {
-    alignItems: 'center',
-    height: '100%',
+  calloutContainer: {
+    backgroundColor: theme.color.translucid,
+    borderRadius: 16,
+    height: 46,
     justifyContent: 'center',
+    paddingHorizontal: 16,
+    width: 160,
+  },
+
+  calloutText: {
+    color: theme.color.primaryDark,
+    fontFamily: theme.font.bold,
+    fontSize: 14,
+  },
+
+  container: {
+    flexGrow: 1,
+  },
+
+  footer: {
+    alignItems: 'center',
+    backgroundColor: theme.color.primaryDark,
+    borderRadius: 20,
+    bottom: 32,
+    elevation: 3,
+    flexDirection: 'row',
+    height: 56,
+    justifyContent: 'center',
+    left: 24,
+    position: 'absolute',
+    right: 24,
+  },
+
+  footerText: {
+    color: theme.color.white,
+    fontFamily: theme.color.bold,
+    fontSize: 20,
+    marginRight: 16,
+  },
+
+  map: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
   },
 })
