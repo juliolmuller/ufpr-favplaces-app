4ac212724b0658d1faab2b6a86f55a8e54272877
Julio Muller <juliodolago@gmail.com>
2021-03-03 23:04:37 -0300

    Created application root

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import AppLoading from 'expo-app-loading'
+import { StatusBar } from 'expo-status-bar'
+import { useFonts } from 'expo-font'
+import {
+  Exo2_400Regular as Exo,
+  Exo2_700Bold as ExoBold,
+  Exo2_600SemiBold as ExoSemiBold,
+  Exo2_700Bold_Italic as ExoBoldItalic,
+  Exo2_600SemiBold_Italic as ExoSemiBoldItalic,
+  Exo2_400Regular_Italic as ExoItalic,
+} from '@expo-google-fonts/exo-2'
+import Routes from './routes'
+
+function App() {
+  const [fontsLoaded] = useFonts({ Exo, ExoBold, ExoSemiBold, ExoBoldItalic, ExoSemiBoldItalic, ExoItalic })
+
+  return (
+    <Choose>
+      <When condition={fontsLoaded}>
+        <AppLoading />
+      </When>
+      <Otherwise>
+        <Routes />
+        <StatusBar style="light" />
+      </Otherwise>
+    </Choose>
+  )
+}
+
+export default App
