755e5ec33c742fb3b6a6c2fc3e4e73ed4cfe5fc7
Julio Muller <juliodolago@gmail.com>
2021-03-06 03:19:09 -0300

    Created view to create/edit favorite places

--- src/components/PlaceForm/index.jsx
+++ src/components/PlaceForm/index.jsx
@@ -1,12 +1,90 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
+import * as ImagePicker from 'expo-image-picker'
+import { Feather } from '@expo/vector-icons'
+import { usePlacesApi } from '../../hooks'
 import styles from './styles'
 
 function PlaceForm() {
+  const place = useRoute().params
+  const { navigate } = useNavigation()
+  const { savePlace } = usePlacesApi()
+
+  const [name, setName] = useState(place.name ?? '')
+  const [about, setAbout] = useState(place.about ?? '')
+  const [photos, setPhotos] = useState(place.photos ?? [])
+
+  async function handleBrowsePhoto() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (status !== 'granted') {
+      alert('Ops! Você precisa fornecer uma foto para cadastra o orfanato.')
+      return
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaType: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.cancelled) {
+      setPhotos([...photos, result.uri])
+    }
+  }
+
+  async function handleFormSubmit() {
+    try {
+      await savePlace({ ...place, name, about, photos })
+
+      navigate('PlacesMap')
+    } catch (error) {
+      console.error(error, { ...error })
+      alert('Falha ao tentar salvar os dados. Tente novamente mais tarde.')
+    }
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.wrapper} contentContainerStyle={styles.container}>
+      <Text style={styles.label}>Nome</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+
+      <Text style={styles.label}>Sobre</Text>
+      <TextInput
+        style={[styles.input, styles.inputMulti]}
+        multiline
+        value={about}
+        onChangeText={setAbout}
+      />
+
+      <Text style={styles.label}>Fotos</Text>
+      <View style={styles.selectedPhotosContainer}>
+        <For each="photo" index="index" of={place.photos}>
+          <Image
+            key={index}
+            style={styles.selectedPhoto}
+            source={{ uri: photo }}
+          />
+        </For>
+      </View>
+      <TouchableOpacity style={styles.photosInput} onPress={handleBrowsePhoto}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </TouchableOpacity>
+
+      <RectButton
+        style={[styles.submitBtn, name ? styles.btnEnabled : styles.btnDisabled]}
+        onPress={handleFormSubmit}
+        enabled={Boolean(name)}
+      >
+        <Text style={styles.SubmitBtnText}>Cadastrar</Text>
+      </RectButton>
+    </ScrollView>
   )
 }
 
--- src/components/PlaceForm/styles.js
+++ src/components/PlaceForm/styles.js
@@ -1,9 +1,81 @@
 import { StyleSheet } from 'react-native'
+import * as theme from '../../theme'
 
 export default StyleSheet.create({
-  wrapper: {
+  SubmitBtnText: {
+    color: theme.color.white,
+    fontFamily: theme.font.bold,
+    fontSize: 16,
+  },
+
+  btnDisabled: {
+    backgroundColor: theme.color.primary,
+  },
+
+  btnEnabled: {
+    backgroundColor: theme.color.primaryLight,
+  },
+
+  container: {
+    padding: 24,
+  },
+
+  input: {
+    backgroundColor: theme.color.white,
+    borderColor: theme.color.whitish,
+    borderRadius: 20,
+    borderWidth: 1.4,
+    height: 56,
+    marginBottom: 16,
+    paddingHorizontal: 24,
+    paddingVertical: 18,
+    textAlignVertical: 'top',
+  },
+
+  inputMulti: {
+    height: 110,
+  },
+
+  label: {
+    color: theme.color.font,
+    fontFamily: theme.font.semiBold,
+    marginBottom: 12,
+    marginTop: 16,
+  },
+
+  photosInput: {
     alignItems: 'center',
-    height: '100%',
+    backgroundColor: theme.color.translucid,
+    borderColor: theme.color.primaryLight,
+    borderRadius: 20,
+    borderStyle: 'dashed',
+    borderWidth: 1.4,
+    height: 56,
     justifyContent: 'center',
+    marginBottom: 32,
+  },
+
+  selectedPhoto: {
+    borderRadius: 20,
+    height: 64,
+    marginBottom: 32,
+    marginRight: 8,
+    width: 64,
+  },
+
+  selectedPhotosContainer: {
+    flexDirection: 'row',
+  },
+
+  submitBtn: {
+    alignItems: 'center',
+    borderRadius: 20,
+    height: 56,
+    justifyContent: 'center',
+    marginTop: 32,
+  },
+
+  wrapper: {
+    flex: 1,
   },
 })
