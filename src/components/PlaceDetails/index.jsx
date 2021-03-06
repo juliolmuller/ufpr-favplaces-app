07a3d11b3e9c9d006b1fe0defab2c038efe53433
Julio Muller <juliodolago@gmail.com>
2021-03-06 01:42:19 -0300

    Created screen to display place details

--- src/components/PlaceDetails/index.jsx
+++ src/components/PlaceDetails/index.jsx
@@ -1,12 +1,68 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Image, Linking, ScrollView, Text, View } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import mapMarker from '../../assets/map-marker.png'
 import styles from './styles'
 
 function PlaceDetails() {
+  const place = useRoute().params
+
+  const handleOpenGoogleMaps = () => {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${place.latitude},${place.longitude}`)
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <Text>PlaceDetails</Text>
-    </View>
+    <ScrollView style={styles.wrapper}>
+      <If condition={place.photos.length}>
+        <View style={styles.imagesContainer}>
+          <ScrollView horizontal pagingEnabled>
+            <For each="photo" index="index" of={place.photos}>
+              <Image
+                key={index}
+                style={styles.image}
+                source={{ uri: photo }}
+              />
+            </For>
+          </ScrollView>
+        </View>
+      </If>
+
+      <View style={styles.detailsContainer}>
+        <If condition={place.about}>
+          <Text style={styles.title}>{place.name}</Text>
+          <Text style={styles.description}>{place.about}</Text>
+        </If>
+
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.mapStyle}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+            initialRegion={{
+              latitude: place.latitude,
+              longitude: place.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+          >
+            <Marker
+              icon={mapMarker}
+              coordinate={{ latitude: place.latitude, longitude: place.longitude }}
+            />
+          </MapView>
+
+          <TouchableOpacity style={styles.routesContainer} onPress={handleOpenGoogleMaps}>
+            <Text style={styles.routesText}>
+              Ver rotas no Google Maps
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -1,9 +1,62 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
+import * as theme from '../../theme'
 
 export default StyleSheet.create({
-  wrapper: {
+  description: {
+    color: theme.color.font,
+    fontFamily: theme.font.semiBold,
+    lineHeight: 24,
+    marginTop: 16,
+  },
+
+  detailsContainer: {
+    padding: 24,
+    paddingBottom: 54,
+  },
+
+  image: {
+    height: 240,
+    resizeMode: 'cover',
+    width: Dimensions.get('window').width,
+  },
+
+  imagesContainer: {
+    height: 240,
+  },
+
+  mapContainer: {
+    backgroundColor: theme.color.background,
+    borderColor: theme.color.border,
+    borderRadius: 20,
+    borderWidth: 1.2,
+    elevation: 10,
+    marginTop: 40,
+    overflow: 'hidden',
+  },
+
+  mapStyle: {
+    height: 150,
+    width: '100%',
+  },
+
+  routesContainer: {
     alignItems: 'center',
-    height: '100%',
     justifyContent: 'center',
+    padding: 16,
+  },
+
+  routesText: {
+    color: theme.color.primaryDark,
+    fontFamily: theme.font.bold,
+  },
+
+  title: {
+    color: theme.color.font,
+    fontFamily: theme.font.bold,
+    fontSize: 28,
+  },
+
+  wrapper: {
+    flex: 1,
   },
 })
